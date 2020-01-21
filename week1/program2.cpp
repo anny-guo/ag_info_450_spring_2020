f600793245db1f1cb6b764533d1acb43897b30a6
anny-guo <59743631+anny-guo@users.noreply.github.com>
2020-01-20 19:09:14 -0500

    Add files via upload

---
+++ week1/program2.cpp
@@ -0,0 +1,16 @@
+// Program #2 - Using a variable
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x; 
+    
+    x = 1023; 
+
+    cout << "This program prints the value of x: ";
+    cout << x; 
+
+    return 0;
+}
