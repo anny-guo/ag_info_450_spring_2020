0198df6554f4e7c9de9eaaf4f8db1a897ef18c54
anny-guo <59743631+anny-guo@users.noreply.github.com>
2020-02-06 19:50:28 -0500

    Create circumference.cpp

---
+++ week3/circumference.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+const float pI = 3.14159265358979323846;
+
+int main()
+{
+    double circumference; 
+    double radius;
+    circumference = 2 * pI * radius;
+    return circumference;
+}
+
+double calculateCircumference (double radius)
+{
+    cout << "Enter radius: "<< endl;
+    cin >> radius;
+    return radius;
+}
