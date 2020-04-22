723e05b29c58f6b21b2c79623eea2f4100dddfea
anny-guo <59743631+anny-guo@users.noreply.github.com>
2020-04-21 21:33:42 -0400

    Update random_write.cpp

--- week11/random_write.cpp
+++ week11/random_write.cpp
@@ -7,15 +7,34 @@ using namespace std;
 
 int main()
 {
-    int numbers = rand() % 1000 + 1;
+    int n = 0;
+    int min = -10000;
+    int max = 10000;
+    int n1 = 0;
+    srand(time(NULL));
 
-    srand(time(0));
-
-    for (int n = 0; -1000 < n < 1000; n++)   
+    ofstream out("randoms.txt");
+    if (!out)
     {
-        numbers = rand() % 1000 + 1;
-
+        cout << "Cannot open file." << endl;
+        return 1;
     }
+
+    for (int n1 = 0; n1 < 1000; n1++)   
+    {
+        int n = rand() % 19999 - 9999;
+        if (n >= 10000 || n <= -10000)
+        {
+            cout << "ERROR WHILE GENERATING NUMBER: " << n << endl;
+            break;
+        }
+
+    if (n < min) min = n;
+    if (n > max) max = n;
+    if (n1 >= 1000) continue;
+    out << n << endl;
+    }
+    out.close();
     return 0;
     
 };
