28ff8fe848c68f3c215d514498605b3e9c4fa10c
anny-guo <59743631+anny-guo@users.noreply.github.com>
2020-05-04 22:32:11 -0400

    Update practice2.cpp

--- week2/practice2.cpp
+++ week2/practice2.cpp
@@ -6,16 +6,33 @@ When the user enters a 0, then execution stops.
 */
 
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
-int main()
-{
-    int value;
-    cout << "Please enter a value: " << endl;
-    cin >> value; //accepts values from user
-    if (value % 1 && value % value ==0 ) //determine if value is prime
-    {
-        cout << "Your value: " << endl; //print result
+int main(){
+    int user_input = 0;
+    bool prime = 0;
+
+    while (1){
+    cout << "Please enter a number " << endl;
+    cin >> user_input;
+        if (user_input == 0){
+            break;
+        }
+    for (int num = 0; num < user_input; num++){
+        if (user_input % num == 0){
+            prime = 0;
+            }
+        if (prime){
+            cout << user_input << " is a prime number!" << endl;
+            }
+        else {
+        cout << user_input << " is NOT a prime number!" << endl;
+            }
+        }
     }
+    cout << "Exectution stopped because you entered 0." << endl;
     return 0;
+
 }
